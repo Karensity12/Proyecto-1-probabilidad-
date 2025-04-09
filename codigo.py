2866c698484ab57a5435d1f7e14aec90fa4bec29
alejandroloturco <alejandrolotruco@udea.edu.co>
2025-04-09 00:21:35 -0500

    arreglo df

--- codigo.py
+++ codigo.py
@@ -4,40 +4,75 @@ import numpy as np
 import seaborn as sns
 from scipy import stats
 
-#Inspección de la base de datos 
-aqui = {'Max1R13_1':3,'Max1R13_2':31,'Max1R13_3':60,'Max1R13_4':88,'aveOralF':114 ,'aveOralM':115,'Gender':116, 'Age':117, 'Ethnicity':118,'T_atm':119 , 'Humidity':120,'Cosmetics':122}
+#%%
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from scipy import stats
+
+def convertir_NAN_MEAN(nombres,df):
+    """Convierte los valores NaN de una columna a su media."""
+    for i in nombres:
+        df[i] = pd.to_numeric(df[i], errors='coerce')  # Convertir a numérico, forzando NaN donde no se puede
+        df[i].fillna(np.mean(df[i]), inplace=True)
+
+aqui = {'aveOralF':114 ,
+'aveOralM':115,
+'Gender':116, 
+'Age':117, 
+'Ethnicity':118,
+'T_atm':119 , 
+'Humidity':120,
+'Cosmetics':122, 
+'Max1R13_1':3,
+'Max1R13_2':31,
+'Max1R13_3':59,
+'Max1R13_4':87}
+
 csv = pd.read_csv(r'FLIR_groups1and2.csv', delimiter=';')
-csv = csv.iloc[2:].reset_index(drop=True) #Eliminación de las dos primeras filas
-csv['PromMax1R13'] = csv[[csv.columns[aqui['Max1R13_1']], csv.columns[aqui['Max1R13_2']],csv.columns[aqui['Max1R13_3']],csv.columns[aqui['Max1R13_4']]]].astype(float).mean(axis=1)
+csv = csv.iloc[2:].reset_index(drop=True)
+
+
 csv_filter = csv.iloc[:, [aqui['aveOralF'], aqui['aveOralM'], aqui['Gender'], aqui['Age'], aqui['Ethnicity'], aqui['T_atm'], aqui['Humidity'], aqui['Cosmetics']]]
-csv_filter.columns = ['aveOralF','aveOralM', 'Gender', 'Age', 'Ethnicity', 'T_atm', 'Humidity', 'Cosmetics']
-csv_filter.loc[:,'PromMax1R13'] = csv['PromMax1R13']
-#df_filled = csv_filter.fillna('nan')
+csv_filter.columns = ["aveOralF","aveOralM", "Gender", "Age", "Ethnicity", "T_atm", "Humidity", "Cosmetics"]
+
+data_temp = csv.iloc[:, [aqui['Max1R13_1'], aqui['Max1R13_2'], aqui['Max1R13_3'], aqui['Max1R13_4']]].astype(float)
+data_temp.columns = ["Max1R13_1","Max1R13_2","Max1R13_3","Max1R13_4"]
+convertir_NAN_MEAN(data_temp.columns,data_temp) 
+
+csv_filter['PromMax1R13'] = data_temp.mean(axis=1)
+
+csv_filter["Cosmetics"] = csv_filter["Cosmetics"].fillna('Nan')
+
+csv_filter.to_csv('FLIR_groups1and2_replace.csv',  sep=';', decimal=',',index=False)
+
+#%%
+#Media, mediana, moda, SD, MAD, varianza, IQR, CV, CVM para aveOralF
 
 
-#csv_filter = csv_filter.replace(' ', np.non)
-csv_filter.loc[:,"Cosmetics"] = csv_filter["Cosmetics"].where(pd.notna(csv_filter["Cosmetics"]), None)
-csv_filter.to_csv('FLIR_groups1and2_replace.csv', index=False)
-
+#%%
 #Funcion para calcular estadisticas
-def informacion(dat):
-    dat = dat.dropna()
+def informacion(dat,categorico=False):
     lista = []
     try:
-        # Intentamos tratar los datos como numéricos
-        dat = pd.to_numeric(dat)
-        lista.append(np.mean(dat))  # media
-        lista.append(np.median(dat))  # mediana
-        moda = dat.mode().iloc[0] if not dat.mode().empty else np.nan
-        lista.append(moda)
-        lista.append(np.std(dat))  # SD
-        lista.append(np.mean(np.abs(dat - np.median(dat))))  # MAD
-        lista.append(np.var(dat))  # Varianza
-        Q1 = np.nanpercentile(dat, 25)
-        Q3 = np.nanpercentile(dat, 75)
-        lista.append(Q3 - Q1)  # IQR
-        lista.append((np.std(dat, ddof=1)) / np.mean(dat) * 100)  # CV
-        lista.append(np.mean(np.abs(dat - np.median(dat))) / np.median(dat) * 100)  # CVM
+        if categorico ==False:
+            # Intentamos tratar los datos como numéricos
+            dat = pd.to_numeric(dat)
+            lista.append(np.mean(dat))  # media
+            lista.append(np.median(dat))  # mediana
+            moda = dat.mode().iloc[0] if not dat.mode().empty else np.nan
+            lista.append(moda)
+            lista.append(np.std(dat))  # SD
+            lista.append(np.mean(np.abs(dat - np.median(dat))))  # MAD
+            lista.append(np.var(dat))  # Varianza
+            Q1 = np.nanpercentile(dat, 25)
+            Q3 = np.nanpercentile(dat, 75)
+            lista.append(Q3 - Q1)  # IQR
+            lista.append((np.std(dat, ddof=1)) / np.mean(dat) * 100)  # CV
+            lista.append(np.mean(np.abs(dat - np.median(dat))) / np.median(dat) * 100)  # CVM
+        elif categorico == True:
+            print
     except:
         # Si falla, es porque son datos categóricos
         for _ in range(2): lista.append(np.nan)  # media, mediana
@@ -78,41 +113,17 @@ print("\nResumen estadístico:")
 print(df_estadisticas)
 
 
+'''resultados = {'': ["Media", "Mediana", "Moda", "SD", "MAD", "Varianza", "IQR", "CV", "CVM"],    
+    'aveOralF': informacion(csv_filter['aveOralF']),
+    'aveOralM': informacion(csv_filter['aveOralM']),
+    'Gender': informacion(csv_filter['Gender']),
+    'Age': informacion(csv_filter['Age']),
+    'Ethnicity': informacion(csv_filter['Ethnicity']),
+    'T_atm': informacion(csv_filter['T_atm']),
+    'Humidity': informacion(csv_filter['Humidity']),
+    'Cosmetics': informacion(csv_filter['Cosmetics'])
+}
 
-#df = pd.DataFrame(resultados)
-#print(df)
-
-
-# analisis_aveOralF = informacion(csv_filter['aveOralF'])
-# analisis_aveOralM = informacion(csv_filter['aveOralM'])
-# analisis_Gender = informacion(csv_filter['Gender'])
-# analisis_Age = informacion(csv_filter['Age'])
-# analisis_Ethnicity = informacion(csv_filter['Ethnicity'])
-# analisis_T_atm = informacion(csv_filter['T_atm'])
-# analisis_Humidity = informacion(csv_filter['Humidity'])
-# analisis_Cosmetics = informacion(csv_filter['Cosmetics'])
-
-# df = pd.DataFrame({
-#     'Media': analisis_aveOralF[0],
-#     'Mediana': analisis_aveOralF[1],
-#     'Moda': analisis_aveOralF[2],
-#     'SD': analisis_aveOralF[3],
-#     'MAD': analisis_aveOralF[4],
-#     'Varianza': analisis_aveOralF[5],
-#     'IQR': analisis_aveOralF[6],
-#     'CV': analisis_aveOralF[7],
-#     'CVM': analisis_aveOralF[8]
-# })
-
-
-
-
-
-
-
-# csv_filter = csv_filter.replace(' ', np.nan)
-#csv_filter = csv_filter.dropna()
-#seleccion = seleccion.reset_index(drop=True)
-
-
+df = pd.DataFrame(resultados)
+print(df)'''
 
